00eba069a613ed038fcdcb1dce484206feeef452
Dent24 <cljhs90607@gmail.com>
2021-03-21 20:43:09 +0800

    vue v-for

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 Vue.component('todo-item', {
-    template: '<li>This is a todo</li>'
+    template: '<li>{{ title }} - {{ num }}</li>',
+    props: {
+        title: String,
+        num: Number
+    }
 })
 
 var app = new Vue({
     el: '#app',
     data() {
         return {
-            message: 'Hello'
+            message: 'Hello',
+            todoList: [
+                {
+                    title: 'Apple',
+                    num: 2
+                },
+                {
+                    title: 'Orenge',
+                    num: 2
+                },
+                {
+                    title: 'Guava',
+                    num: 2
+                }
+            ]
         }
     }
 })
